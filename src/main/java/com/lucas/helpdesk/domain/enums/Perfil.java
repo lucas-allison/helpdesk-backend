990a39d3554ecd89b588235d2fa2af1258a3ab75
lucas-allison <lucas.allison444@gmail.com>
2025-10-15 16:44:12 -0300

    Inclusão dos enumeradores

---
+++ src/main/java/com/lucas/helpdesk/domain/enums/Perfil.java
@@ -0,0 +1,34 @@
+package com.lucas.helpdesk.domain.enums;
+
+public enum Perfil {
+	ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO (2, "ROLE_TECNICO");
+	
+	public Integer codigo;
+	public String descricao;
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	private Perfil(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}	
+	
+	public static Perfil toEnum(Integer cod) {
+		if (cod == null)
+			return null;
+		
+		for (Perfil x : Perfil.values()) {
+			if (cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Perfil inválido");
+	} 
+	
+}
---
+++ src/main/java/com/lucas/helpdesk/domain/enums/Prioridade.java
@@ -0,0 +1,34 @@
+package com.lucas.helpdesk.domain.enums;
+
+public enum Prioridade {
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+	
+	public Integer codigo;
+	public String descricao;
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	private Prioridade(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}	
+	
+	public static Prioridade toEnum(Integer cod) {
+		if (cod == null)
+			return null;
+		
+		for (Prioridade x : Prioridade.values()) {
+			if (cod.equals(x.getCodigo())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Prioridade inválida");
+	} 
+	
+}
