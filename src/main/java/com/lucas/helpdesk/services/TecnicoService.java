d94dd3c1a3b801078470ca9ad66a999bbf0cbb3b
lucas-allison <lucas.allison444@gmail.com>
2025-10-21 23:37:30 -0300

    Delete para Técnico implementado

--- src/main/java/com/lucas/helpdesk/services/TecnicoService.java
+++ src/main/java/com/lucas/helpdesk/services/TecnicoService.java
@@ -58,4 +58,13 @@ public class TecnicoService {
 		oldObj = new Tecnico(requestModel);
 		return repository.save(oldObj);
 	}
+
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+
+		if (obj.getChamados().size() > 0)
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+
+		repository.deleteById(id);
+	}
 }
