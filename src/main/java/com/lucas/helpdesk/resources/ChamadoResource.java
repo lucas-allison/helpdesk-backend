391478f603fdf75826823581074ef4545804f47f
lucas-allison <lucas.allison444@gmail.com>
2025-10-22 00:26:05 -0300

    Criação das camadas Inclusão da api findById para chamado

---
+++ src/main/java/com/lucas/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,25 @@
+package com.lucas.helpdesk.resources;
+
+import com.lucas.helpdesk.domain.Chamado;
+import com.lucas.helpdesk.dtos.ChamadoDTO;
+import com.lucas.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+        Chamado obj = service.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
---
+++ src/main/java/com/lucas/helpdesk/services/ChamadoService.java
@@ -0,0 +1,20 @@
+package com.lucas.helpdesk.services;
+
+import com.lucas.helpdesk.domain.Chamado;
+import com.lucas.helpdesk.repositories.ChamadoRepository;
+import com.lucas.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}
